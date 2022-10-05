82ddf177e980fedbb0136fcb704ae3468e71ed97
Arthur Arantes Faria <arthurarantes23@hotmail.com>
2022-10-05 00:30:39 -0300

    2720 v3.0

--- 2720.py
+++ 2720.py
@@ -20,7 +20,7 @@
     Curated:
     [
         (3, 2, [(1, 6), (3, 4), (2, 2)]), 
-        (2, 1, [(25, 200), (32, 20)])
+        (2, 1, [(25, 20), (32, 20)])
     ]
 
     Result:
@@ -29,10 +29,46 @@
 
 """
 
+def merge_sort(unsorted_list):
+   if len(unsorted_list) <= 1:
+      return unsorted_list
+
+   middle = len(unsorted_list) // 2
+   left_list = unsorted_list[:middle]
+   right_list = unsorted_list[middle:]
+
+   left_list = merge_sort(left_list)
+   right_list = merge_sort(right_list)
+   return list(merge(left_list, right_list))
+
+
+def merge(left_half,right_half):
+   res = []
+   while len(left_half) != 0 and len(right_half) != 0:      
+        if left_half[0][1] > right_half[0][1]:
+            res.append(left_half[0])
+            left_half.remove(left_half[0])
+        elif left_half[0][1] == right_half[0][1]:
+            if left_half[0][0] < right_half[0][0]:
+                res.append(left_half[0])
+                left_half.remove(left_half[0])
+            else:
+                res.append(right_half[0])
+                right_half.remove(right_half[0])
+        else:
+            res.append(right_half[0])
+            right_half.remove(right_half[0])
+   if len(left_half) == 0:
+      res = res + right_half
+   else:
+      res = res + left_half
+   return res
+
 t = int(input())
 p = [None] * t
+s = []
 
-if t<=20:
+if t<=20:    
     for i in range(t):
         n, k = map(int, input().split())
 
@@ -41,19 +77,18 @@ if t<=20:
                 p[i] = n,k,[]
                 for j in range(n):
                     id,h,w,l = map(int, input().split())
-                    p[i][2].append([id,(h * w * l)])
-
-    for i in range(t):
-        p[i][2].sort(key=lambda x: x[0],reverse=False)
-        p[i][2].sort(key=lambda x: x[1],reverse=True)
-        
-        if p[i][1] == 1:
-            del p[i][2][-(p[i][1]):]
-        else: 
-            del p[i][2][-((p[i][1])-1):]
+                    p[i][2].append((id,(h * w * l)))
 
     for i in range(t):
         r = []
-        for j in range(p[i][1]):
-            r.append(p[i][2][j][0])
+        
+        s.append(merge_sort(p[i][2]))
+        
+        if p[i][1] == 1:
+            del s[i][-(p[i][1]):]
+        else: 
+            del s[i][-((p[i][1])-1):]            
+        
+        for j in range(len(s[i])):
+            r.append(s[i][j][0])
         print(*r, sep = " ")
